93c3e917edebec27b4d7a3eac8f3ccaa9c25a560
Kjudisca Rasjudisca <xaneta@ya.ru>
2018-02-24 14:31:32 +0300

    100 passing (68ms)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+    var result = 0;
+    var aux_number = 1;
+    //result += Math.floor(number/5);
+    while(aux_number * 5 < number){
+        aux_number *= 5;
+        result += Math.floor(number/aux_number);
+    }
+    return result;
 }
